0c6f0892a109739fdb1d507085311beabfaa3ece
Kevin <kevinsantander@tdf.edu.ar>
2022-09-20 00:01:28 -0300

    segundo commit

---
+++ holaMundo.js
@@ -0,0 +1,19 @@
+//console.log('hola desde Node');
+
+/*let readlineSync = require('readline-sync');
+let alturaPrenosa = readlineSync.question("Indique la altura de la persona: ");
+console.log("su altura es:");
+console.log(alturaPrenosa);*/
+
+/*Ejercicios 1: modifica el primer script "hola mundo para que:El mensaje que se muestra al usuario se almacene en
+una variable llamada mensaje y el funcionamiento del
+script sea el mismo. "*/
+let readlineSync = require('readline-sync');
+
+let mensaje = ("Hola Mundo!");
+console.log(mensaje); 
+
+/*Ejercicio numero 2: Modificar el ejemplo de secuencia: Qué cada mensaje se almacene en una variable a
+mostrar por consola y que el funcionamiento del script sea
+el mismo*/
+
